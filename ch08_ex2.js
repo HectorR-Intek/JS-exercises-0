af34b989dee8a6bd1363bcb3d3d04c9373b8ff8f
HectorR-Intek <hjrodriguez@intekglobal.com>
2025-09-04 17:53:08 -0600

    LCM of three periods is now considered for the cycle

--- ch08_ex2.js
+++ ch08_ex2.js
@@ -1,51 +1,50 @@
-//Create functions A, B, and C that execute every 30s, 1min, and 1min 15s respectively. 
-// Use only 1 timer/interval to control all three functions. 
+//Create functions A, B, and C that execute every 30s, 1min, and 1min 15s respectively.
+// Use only 1 timer/interval to control all three functions.
 "use strict";
-function control(){
-    let unitsOfFifteen = 0;
+function control() {
+  let unitsOfFifteen = 0;
 
-    return function(){
-        unitsOfFifteen++;
-        if(unitsOfFifteen == 11) unitsOfFifteen =  1;
-        caller(unitsOfFifteen);
-        return;
-    }
+  return function () {
+    unitsOfFifteen++; //Cycles each 20 iterations since that equals 300 seconds which is the LCM for 30, 60 and 75.
+    if (unitsOfFifteen === 21) unitsOfFifteen = 1;
+    caller(unitsOfFifteen);
+    return;
+  };
 }
-function thirty(){
-        console.log("Each thirty seconds");
-        return;
-    }
-function sixty(){
-        console.log("Each sixty seconds");
-        return;
-    }
-function seventyfive(){
-        console.log("Each seventyfive seconds");
-        return;
-    }
+function thirty() {
+  console.log("Each thirty seconds");
+  return;
+}
+function sixty() {
+  console.log("Each sixty seconds");
+  return;
+}
+function seventyfive() {
+  console.log("Each seventyfive seconds");
+  return;
+}
 
-function caller(n){
-    const divisors = [2,4,5];
+function caller(n) {
+  const divisors = [2, 4, 5];
 
-    for(let divisor of divisors){
-        if(n%divisor==0){
-            switch(divisor){
-            case 2:
-                thirty();
-                break;
-            case 4:
-                sixty();
-                break;
-            case 5:
-                seventyfive();
-                break;
-            default:
-                return;
-            }
-        }
+  for (let divisor of divisors) {
+    if (n % divisor == 0) {
+      switch (divisor) {
+        case 2:
+          thirty();
+          break;
+        case 4:
+          sixty();
+          break;
+        case 5:
+          seventyfive();
+          break;
+        default:
+          return;
+      }
     }
-    
+  }
 }
 
 const controlFn = control();
-setInterval(controlFn, 1500); // I changed 15 seconds to 1.5 seconds to debug quicker.
+setInterval(controlFn, 15000); // I changed 15 seconds to 1.5 seconds to debug quicker.
