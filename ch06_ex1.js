3ac76e1588375ee422a5307b8c29137ef5965266
HectorR-Intek <hjrodriguez@intekglobal.com>
2025-09-04 17:31:51 -0600

    Area and perimeter now return numerical values only

--- ch06_ex1.js
+++ ch06_ex1.js
@@ -1,57 +1,57 @@
 // Chapter 6, exercise 1:
 "use strict";
-class Shape{
-    constructor(edges, type = "Regular polygon"){
-        this.edges = edges;
-        this.type = type;
-    }
-    display(){
-        return `The ${this.type} has ${this.edges} edges`;
-    }
-    area(){
-        throw new Error("Unimplemented method");
-    }
-    perimeter(){
-        throw new Error("Unimplemented method");
-    }
+class Shape {
+  constructor(edges, type = "Regular polygon") {
+    this.edges = edges;
+    this.type = type;
+  }
+  display() {
+    return `The ${this.type} has ${this.edges} edges`;
+  }
+  area() {
+    throw new Error("Unimplemented method");
+  }
+  perimeter() {
+    throw new Error("Unimplemented method");
+  }
 }
 
-class Circle extends Shape{
-    constructor(radius){
-        super(0, "Circle");
-        this.radius = radius;
-    }
-    area(){
-        return Math.PI*this.radius*this.radius;
-    }
-    perimeter(){
-        return Math.PI*2*this.radius;
-    }
+class Circle extends Shape {
+  constructor(radius) {
+    super(0, "Circle");
+    this.radius = radius;
+  }
+  area() {
+    return Math.PI * this.radius * this.radius;
+  }
+  perimeter() {
+    return Math.PI * 2 * this.radius;
+  }
 }
 
-class Quadrilateral extends Shape{
-    constructor(width, height){
-        super(4, "Quadrilateral");
-        this.width = width;
-        this.height = height;
-    }
-    area(){
-        return `The area is: ${this.width*this.height}`;
-    }
-    perimeter(){
-        return `The perimeter is: ${2*(this.width+this.height)}`;
-    }
+class Quadrilateral extends Shape {
+  constructor(width, height) {
+    super(4, "Quadrilateral");
+    this.width = width;
+    this.height = height;
+  }
+  area() {
+    return this.width * this.height;
+  }
+  perimeter() {
+    return 2 * (this.width + this.height);
+  }
 }
 
-class Triangle extends Shape{
-    constructor(base, height){
-        super(3, "Triangle");
-        this.base = base;
-        this.height = height;
-    }
-    area(){
-        return `The area is: ${(this.base*this.height)/2}`;
-    }
+class Triangle extends Shape {
+  constructor(base, height) {
+    super(3, "Triangle");
+    this.base = base;
+    this.height = height;
+  }
+  area() {
+    return (this.base * this.height) / 2;
+  }
 }
 
 const heptagon = new Shape(7, "Heptagon");
@@ -59,47 +59,48 @@ console.log(heptagon.display());
 
 const circleOne = new Circle(1);
 console.log(circleOne.area());
+console.log(circleOne.perimeter());
 
 const square = new Quadrilateral(5, 5);
 console.log(square.area());
 
 //----------------------------------
 
-class Animal{
-    constructor(species, legs){
-        this.species = species;
-        this.legs = legs;
-    }
-    makeSound(){
-        return `The ${this.species} makes a sound`;
-    }
+class Animal {
+  constructor(species, legs) {
+    this.species = species;
+    this.legs = legs;
+  }
+  makeSound() {
+    return `The ${this.species} makes a sound`;
+  }
 }
 
-class Dog extends Animal{
-    constructor(species = "Dog"){
-        super(species, 4);
-    }
-    makeSound(){
-        return `The ${this.species} barks`;
-    }
+class Dog extends Animal {
+  constructor(species = "Dog") {
+    super(species, 4);
+  }
+  makeSound() {
+    return `The ${this.species} barks`;
+  }
 }
 
-class Cat extends Animal{
-    constructor(species = "Cat"){
-        super(species, 4);
-    }
-    makeSound(){
-        return `The ${this.species} meows`;
-    }
+class Cat extends Animal {
+  constructor(species = "Cat") {
+    super(species, 4);
+  }
+  makeSound() {
+    return `The ${this.species} meows`;
+  }
 }
 
-class Duck extends Animal{
-    constructor(species = "Duck"){
-        super(species, 2);
-    }
-    makeSound(){
-        return `The ${this.species} quacks`;
-    }
+class Duck extends Animal {
+  constructor(species = "Duck") {
+    super(species, 2);
+  }
+  makeSound() {
+    return `The ${this.species} quacks`;
+  }
 }
 
 const fido = new Dog();
